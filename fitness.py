6752cbaf2a89fa52391f8524e129d6bdf0deed18
Aldo <aldo.fsmonteiro@gmail.com>
2018-11-17 22:26:16 -0300

    Fitness functions

--- fitness.py
+++ fitness.py
@@ -1,5 +1,4 @@
 import numpy as np
-import utils
 from utils.attribute_utils import evadido, diasAposEvasao, periculosidade,\
     prioritario, qtdProcessos, totalVitmasConsumado, totalVitmasTentado,\
     prisaoPreventiva, prisaoCondenatoria, mpCVLI, enderecoConhecido,\
@@ -39,3 +38,45 @@ class FuncaoViabilidade:
         SC = carcerario(row)
         Xv = np.array([EC, FT, RG, PD, 1-np.tanh(Texp), MA, SC])
         return np.average(Xv, weights=self.pesos)
+
+class FitnessMult:
+    def __init__(self, Cg, Cv, Cind, Kr, Kv, dataset):
+        self.Cg = Cg
+        self.Cind = Cind
+        self.Cv = Cv
+        self.G = lambda x : 0 # falta implementar
+        self.R = FuncaoRelevancia(Kr)
+        self.V = FuncaoViabilidade(Kv)
+        self.dataset = dataset
+
+    def _fitness_ind(self, row):
+        relevancy = self.R(row)
+        viability = self.V(row)
+        return (1-np.exp(-viability/self.Cv))*relevancy
+
+    def __call__(self, chromossome):
+        data = self.dataset.iloc[chromossome]
+        if len(data) == 0:
+            return -np.inf
+        group = self.G(data)
+        individual = np.mean(data.apply(self._fitness_ind, axis=1).values)
+        return self.Cg*group + self.Cind*individual
+
+class FitnessSum:
+    def __init__(self, Cg, Cr, Cv, Kr, Kv, dataset):
+        self.Cg = Cg
+        self.Cr = Cr
+        self.Cv = Cv
+        self.G = lambda x : 0 # falta implementar
+        self.R = FuncaoRelevancia(Kr)
+        self.V = FuncaoViabilidade(Kv)
+        self.dataset = dataset
+
+    def __call__(self, chromossome):
+        data = self.dataset.iloc[chromossome]
+        if len(data) == 0:
+            return -np.inf
+        grouping = self.G(data)
+        relevancy = np.mean(data.apply(self.R, axis=1).values)
+        viability = np.mean(data.apply(self.V, axis=1).values)
+        return self.Cg*grouping + self.Cr*relevancy + self.Cv*viability
