b9d0065cf9e6a2ebcd484a144d414a3cc7ca9048
Aldo Monteiro <aldo.monteiro@pitang.com>
2019-02-04 20:08:17 -0300

    test progress bar

--- apps/ranking.py
+++ apps/ranking.py
@@ -10,7 +10,7 @@ from matplotlib import pyplot as plt
 from flask import send_from_directory
 
 import dash
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output, State, Event
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
@@ -21,9 +21,38 @@ import numpy as np
 
 from app import app, server
 
+
+from ga.genetic_algorithm import GeneticAlgorithm
+from ga.crossover import Crossover
+from ga.mutation import Mutation
+from ga.selection import Selection
+from fitness import FitnessSum
+import constants
+
 UPLOAD_DIRECTORY = "./datasets"
 
 df = None
+ranking_progress = 0
+
+def rank(dataset, V, R, G, som_attrs, rank_length, pop_size=30, generations_to_run=100):
+    global ranking_progress
+    table_len = dataset.shape[0]
+    gene_set = list(range(table_len))
+    chr_size = rank_length
+
+    selection = Selection(0.5)
+    crossover = Crossover(kind=1)
+    mutation = Mutation(0.5)
+    optimizer = GeneticAlgorithm(gene_set, selection, crossover, mutation)
+    optimizer.initializePopulation(chr_size, pop_size)
+
+    fitness_function = FitnessSum(G, R, V, constants.RELEVANCY_WEIGHTS, constants.VIABILITY_WEIGHTS, dataset)
+
+    for i in range(generations_to_run):
+        optimizer.step(fitness_function)
+        ranking_progress = i/generations_to_run
+    ranking_progress = 1
+    return optimizer
 
 if not os.path.exists(UPLOAD_DIRECTORY):
     os.makedirs(UPLOAD_DIRECTORY)
@@ -35,7 +64,11 @@ def download(path):
 
 
 layout = html.Div([
-    html.H1("..."),
+    dcc.Interval(
+            id='ranking-progress-update',
+            interval=1000
+    ),
+    html.H1("...", id='title-h1'),
     html.Div([
         html.Div([
             html.Div([
@@ -58,7 +91,7 @@ layout = html.Div([
                     },
                     multiple=True,
                 ),
-            ], className='mt-3'),
+            ]),
             html.Div([
                 html.H2("Lista de Tabelas"),
                 html.Div([
@@ -85,6 +118,13 @@ layout = html.Div([
                             dbc.CardHeader('Proximidade'),
                             dbc.CardBody([
                                 dcc.Slider(value=0.5, step=0.01, min=0, max=1, marks={0:'0', 1:'1'}),
+                                dbc.Checklist(
+                                    options=[
+                                        {'label': attr, 'value': attr} for attr in ['IAS']
+                                    ],
+                                    values=[],
+                                    className='mt-3'
+                                )
                             ])
                         ], className='mt-3'),
             ], className='mt-3'),
@@ -98,8 +138,13 @@ layout = html.Div([
                 ], style={'overflowY': 'scroll', 'height': '60vh'})
             ])
         ], className="col-8")
-    ], className="row mt-5")
+    ], className="row mt-5"),
+    html.Div([
+        dbc.Button('Rankear', id='rank-button'),
+        dbc.Progress(value=0, id='ranking-progress'),
+    ], id='button-progress', className='d-flex justify-content-center')
 ], className="container mt-3")
+
 
 def save_file(name, content):
     """Decode and store a file uploaded with Plotly Dash."""
@@ -131,6 +176,28 @@ def file_download_link(filename):
     location = "/download/{}".format(urlquote(filename))
     return html.A(filename, href=filename)
 
+@app.callback(Output('ranking-progress', 'value'),
+              events=[Event('ranking-progress-update', 'interval')])
+def update_progress():
+    global ranking_progress
+    ranking_progress+=1
+    return str(ranking_progress%100)
+
+@app.callback(Output('button-progress', 'children'),
+             [Input('ranking-progress', 'value')])
+def button_progress(progress):
+    global ranking_progress
+    if progress == 0:
+        return  dbc.Button('Rankear', id='rank-button')
+    else:
+        return dbc.Progress(value=0, id='ranking-progress')
+
+@app.callback(Output('title-h1', 'children'),
+             [Input('rank-button', 'n_clicks')])
+def on_rank_btn_click(n_clicks):
+    print(n_clicks)
+    return str(n_clicks)
+
 last_n_clicks = None
 
 @app.callback(
@@ -139,7 +206,11 @@ last_n_clicks = None
 )
 def update_output(uploaded_filenames, uploaded_file_contents, n_clicks):
     """Save uploaded files and regenerate the file list."""
-    if uploaded_filenames is not None and uploaded_file_contents is not None:
+    global last_n_clicks
+    if n_clicks != last_n_clicks:
+        remove_uploaded_files()
+        last_n_clicks = n_clicks
+    elif uploaded_filenames is not None and uploaded_file_contents is not None:
         for name, data in zip(uploaded_filenames, uploaded_file_contents):
             name = 'BD_SITE_SCC_projeto_UPE_unificada.xlsx'
             save_file(name, data)
